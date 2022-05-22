c0615d98bfd64441841e86275faaf5dcd801b35e
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-23 00:40:47 +0600

    banner added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className="carousel carousel-end rounded-box my-12">
+      <div className="carousel-item">
+        <img src="https://images.unsplash.com/photo-1568209865332-a15790aed756?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="Drink" />
+      </div>
+      <div className="carousel-item">
+        <img src="https://images.unsplash.com/photo-1618764400608-9e7115eede74?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="Drink" />
+      </div>
+      <div className="carousel-item">
+        <img src="https://images.unsplash.com/photo-1600176842418-f7c1d1035ef1?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876" alt="Drink" />
+      </div>
+     
+    </div>
+  );
+};
+
+export default Banner;
