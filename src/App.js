638142787b0fbd91549750dbe64cf2e328368142
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-23 21:32:25 +0600

    make admin problem

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Login from './Pages/Login/Login';
 import { ToastContainer } from 'react-toastify';
 import ServiceDetails from './Pages/Purchase/ServiceDetails';
 import RequireAuth from './Pages/Shared/RequireAuth';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import AddReview from './Pages/Dashboard/AddReview';
+import MyProfile from './Pages/Dashboard/MyProfile';
+import AddProduct from './Pages/Dashboard/AddProduct';
+import MakeAdmin from './Pages/Dashboard/MakeAdmin';
+import MyOrders from './Pages/Dashboard/MyOrders';
+import ManageAllOrders from './Pages/Dashboard/ManageAllOrders';
+import ManageProducts from './Pages/Dashboard/ManageProducts';
+import RequireAdmin from './Pages/Shared/RequireAdmin';
 
 function App() {
   return (
@@ -18,7 +27,16 @@ function App() {
           <ServiceDetails/>
         </RequireAuth>
         }/>
-        {/* <Route path="about" element={<About />} /> */}
+         <Route path="dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} >
+          <Route index element={<MyOrders></MyOrders>}></Route>
+          <Route path="review" element={<AddReview></AddReview>}></Route>
+          <Route path="myprofile" element={<MyProfile></MyProfile>}></Route>
+          {/* <Route path="payment/:id" element={<Payment></Payment>}></Route> */}
+          <Route path="orders" element={<RequireAdmin><ManageAllOrders></ManageAllOrders></RequireAdmin>}></Route>
+          <Route path="addproduct" element={<RequireAdmin><AddProduct></AddProduct></RequireAdmin>}></Route>
+          <Route path="manageproduct" element={<RequireAdmin><ManageProducts></ManageProducts></RequireAdmin>}></Route>
+          <Route path="makeadmin" element={<MakeAdmin></MakeAdmin>}></Route>
+        </Route>
         <Route path="login" element={<Login />} />
       </Routes> 
       <ToastContainer />
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../Hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
-        <div>
-            <h2>Dashboard</h2>
+        <div class="drawer drawer-mobile">
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content">
+                <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
+                <Outlet></Outlet>
+            </div>
+            <div class="drawer-side">
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+                    {/* <!-- Sidebar content here --> */}
+                    <li><Link to="/dashboard">My Orders</Link></li>
+                    <li><Link to="/dashboard/review">Add a Review</Link></li>
+                    <li><Link to="/dashboard/myprofile">My Profile</Link></li>
+                    <li><Link to="/dashboard/makeadmin">Make Admin</Link></li>
+                    { admin && <>
+                        
+                        <li><Link to="/dashboard/addproduct">Add a Product</Link></li>
+                        <li><Link to="/dashboard/manageproduct">Manage Products</Link></li>
+                    </>}
+                </ul>
+
+            </div>
         </div>
     );
 };
